98098fc7072a02ee3eeeaac0bbd21fa28926aaaa
Tobias Megies <megies@geophysik.uni-muenchen.de>
2015-03-17 11:33:14 +0100

    try to get appveyor to actually install obspy, don't upload packages to main channel for now

--- continuous-integration/binstar-push.py
+++ continuous-integration/binstar-push.py
@@ -4,7 +4,7 @@ import subprocess
 import traceback
 
 token = os.environ['BINSTAR_TOKEN']
-cmd = ['binstar', '-t', token, 'upload', '--force']
+cmd = ['binstar', '-t', token, 'upload', '--channel', 'appveyor', '--force']
 cmd.extend(glob.glob('*.tar.bz2'))
 try:
     subprocess.check_call(cmd)
